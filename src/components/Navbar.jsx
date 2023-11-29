dc7e09769ac95d3b3693bff1a930664ec5983c3d
Ashley Rivera <ashleyariv@gmail.com>
2023-11-28 21:34:38 -0500

    background updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -53,8 +53,11 @@ const Navbar = () => {
                   key = {link.id}
                   className = {`${
                     active === link.title?'text-white':'text-secondary'
-                  } hover:text-white text-[18px] font-medium cursor-pointer`}
-                  onClick = {() => setActive(link.title)}
+                  } font-poppins font-medium cursor-pointer text-[16px]`}
+                  onClick = {() => {
+                    setToggle(!toggle)
+                    setActive(link.title)
+                  }}
                   >
                     <a href = {`${link.id}`}>{link.title}</a>
                   </li>
