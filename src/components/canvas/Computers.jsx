46fb5b0281f2ab825c6e6692e6c0caa034b5ec5f
Ashley Rivera <ashleyariv@gmail.com>
2023-12-12 14:10:35 -0500

    attempting to finish computer model

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -10,16 +10,29 @@ const Computers = () => {
   return (
     <mesh>
       <hemisphereLight intensity = {0.15} groundColor = 'black' />
+      <spotLight 
+        position = {[-20, 50, 10]}
+        angle = {0.12}
+        penumbra = {1}
+        intensity = {1}
+        castShadow
+        shadow-mapSize = {1024}
+      />
       <pointLight intensity = {1} />
-      <primitive object = {computer.scene} />
+      <primitive
+        object = {computer.scene} 
+        scale = {0.75}
+        position = {[0, -3.25, -1.5]}
+        rotation = {[-0.01, -0.2, -0.1]}
+      />
     </mesh>
   )
 }
 
-const ComputerCanvas = () => {
+const ComputersCanvas = () => {
   return (
     <Canvas
-      frameLoop = 'demand'
+      frameloop = 'demand'
       shadows
       camera = {{position: [20,3,5], fov: 25}}
       gl = {{preserveDrawingBuffer: true}}
@@ -27,8 +40,8 @@ const ComputerCanvas = () => {
       <Suspense fallback = {<CanvasLoader />}>
         <OrbitControls 
           enableZoom = {false}
-          MaxPolarAngle = {Math.PI / 2}
-          MinPolarAngle = {Math.PI / 2}
+          maxPolarAngle = {Math.PI / 2}
+          minPolarAngle = {Math.PI / 2}
         />
         <Computers />
       </Suspense>
@@ -37,4 +50,4 @@ const ComputerCanvas = () => {
   )
 }
 
-export default Computers
+export default ComputersCanvas
